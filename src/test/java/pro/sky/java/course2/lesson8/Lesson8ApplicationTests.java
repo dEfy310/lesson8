f4fec1749946012a443f14922b37933467de20a9
dEfy310 <musinov_2004@mail.ru>
2022-02-17 21:41:10 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/lesson8/Lesson8ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.lesson8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
